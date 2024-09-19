7f8bd105ecc056581494b44e3978323f9b1953d0
jaejung <podosi158@gmail.com>
2024-09-19 18:19:49 +0900

    Project start

---
+++ src/main/java/com/example/virtualmoney_data_alarm_service/VirtualMoneyDataAlarmServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.virtualmoney_data_alarm_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+    public class VirtualMoneyDataAlarmServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VirtualMoneyDataAlarmServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/virtualmoney_data_alarm_service/VirtualMoneyDataAlarmServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.virtualmoney_data_alarm_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualMoneyDataAlarmServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
